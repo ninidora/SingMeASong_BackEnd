56869de2804d5deec4ef32fb68fbe274b424a00c
Pedro Lucas <pedrolucaspina22@gmail.com>
2021-12-03 15:41:36 -0300

    feat: add recommendations repository

---
+++ src/repositories/recommendationsRepository.js
@@ -0,0 +1,23 @@
+import connection from '../database.js';
+
+const InsertRecommendation = async (name, artist, link) => {
+    const result = await connection.query(`INSERT INTO musics (name, artist, link, score)
+        VALUES ($1, $2, $3, $4)`, [name, artist, link, 0]);
+
+    return result;
+};
+
+const SelectVotedRecommendation = async (id) => {
+    const result = await connection.query('SELECT * FROM musics WHERE id = $1', [id]);
+    return result;
+};
+
+const UpdateRecommendationsScore = async (id, newScore) => {
+    await connection.query('UPDATE musics SET score = $1 WHERE id = $2', [newScore, id]);
+};
+
+export {
+    InsertRecommendation,
+    SelectVotedRecommendation,
+    UpdateRecommendationsScore,
+};
---
+++ test/unit/postRecomendations.test.js
@@ -0,0 +1,4 @@
+
+describe('post /recommendations', () => {
+
+})
